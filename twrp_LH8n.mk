3284943ad746d64cf17430f3d17314d216119225
nazephyrus <126683488+naden01@users.noreply.github.com>
2024-08-15 02:50:08 -0700

    Update twrp_LH8n.mk

--- twrp_LH8n.mk
+++ twrp_LH8n.mk
@@ -9,11 +9,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
-# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
-
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
+
+# Configure launch_with_vendor_ramdisk.mk
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
+
+# Configure emulated_storage.mk
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
 # Inherit from TECNO-LH8n device
 $(call inherit-product, device/tecno/LH8n/device.mk)
